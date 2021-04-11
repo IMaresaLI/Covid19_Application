b3ee22fafcb91247f66357ca77b54eafb88430a6
Batuhan Olgaç <70446049+IMaresaLI@users.noreply.github.com>
2021-04-11 21:55:27 +0300

    Update DatabaseManager.py

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -12,25 +12,25 @@ class sqliteData(IDatabase):
         self.tblName = tableName
 
     def Connect(self):
-        global list
         connection = sqlite3.connect("covidDataset.db")
         cursor = connection.cursor()
         list = [connection,cursor]
+        return list
 
 
     def Add(self,Current_Case,RYCase,Death,RYDeath,Total_Doses,RYDoses,Vaccinated,RYPercent):
-        self.Connect()
-        list[1].execute(f"INSERT INTO {self.tblName} (Current_Case,Reported_Yesterday_Case,Death,Reported_Yesterday_Death,Total_Doses,Reported_Yesterday_Doses,Vaccinated,Reported_Yesterday_percent) VALUES (?,?,?,?,?,?,?,?)",
+        conn = self.Connect()
+        conn[1].execute(f"INSERT INTO {self.tblName} (Current_Case,Reported_Yesterday_Case,Death,Reported_Yesterday_Death,Total_Doses,Reported_Yesterday_Doses,Vaccinated,Reported_Yesterday_percent) VALUES (?,?,?,?,?,?,?,?)",
                        (Current_Case, RYCase, Death, RYDeath,Total_Doses,RYDoses,Vaccinated,RYPercent))
-        list[0].commit()
-        list[0].close()
+        conn[0].commit()
+        conn[0].close()
         print("işlem Tamamlandı.")
 
     def getData(self):
-        self.Connect()
-        list[1].execute(f"Select * from {self.tblName}")
-        data = list[1].fetchall()
-        list[0].close()
+        conn = self.Connect()
+        conn[1].execute(f"Select * from {self.tblName}")
+        data = conn[1].fetchall()
+        conn[0].close()
         return data
 
 
