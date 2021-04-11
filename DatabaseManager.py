3750def178cc172543ce7b088f2f772d57ba4cfd
Batuhan Olgaç <70446049+IMaresaLI@users.noreply.github.com>
2021-04-11 21:51:56 +0300

    Update DatabaseManager.py

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -26,42 +26,13 @@ class sqliteData(IDatabase):
         list[0].close()
         print("işlem Tamamlandı.")
 
-    # def Update(self,servName,ip,port,status):
-    #     self.Connect()
-    #     list[1].execute(f"Update {self.tblName} set ServerName=?,Host=?,Port=?,Status=? Where Port=?",
-    #                     (servName,ip,port,status))
-    #     list[0].commit()
-    #     list[0].close()
-
-    # def UpdateStatus(self,Status,Port):
-    #     self.Connect()
-    #     list[1].execute(f"Update {self.tblName} set Status=? Where Port=?",
-    #                     (Status,Port))
-    #     list[0].commit()
-    #     list[0].close()
-
-
-    def Delete(self,Port):
-        self.Connect()
-        list[1].execute(f"Delete from {self.tblName} Where Port=?",
-                        (Port,))
-        list[0].commit()
-        list[0].close()
-
-
     def getData(self):
         self.Connect()
         list[1].execute(f"Select * from {self.tblName}")
         data = list[1].fetchall()
         list[0].close()
         return data
-    
-    def getData2(self,port):
-        self.Connect()
-        list[1].execute(f"Select * from {self.tblName} Where port=?",(port,))
-        data = list[1].fetchone()
-        list[0].close()
-        return data
+
 
 
 
